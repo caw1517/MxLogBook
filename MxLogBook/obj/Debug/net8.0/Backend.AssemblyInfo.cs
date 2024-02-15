b7285f7a9add528640a12ee511378cce77dce167
caw1517 <canaan.white5@gmail.com>
2024-02-15 10:44:08 -0700

    Updated gitignore

--- MxLogBook/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ MxLogBook/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3941bb9d2a26e7d8dac03f241b0b78a598154a10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f327d09a9896e27029affbddd17fbc9ef5254b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
